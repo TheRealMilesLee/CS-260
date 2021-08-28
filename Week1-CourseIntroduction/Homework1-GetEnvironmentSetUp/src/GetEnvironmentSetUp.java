0da7cd992d44579718b677d58bebe5098274c633
Serendipity <hl3265@truman.edu>
2021-08-28 11:52:16 -0500

    现在问题在没办法跑完，Vector总是爆掉

--- Week1-CourseIntroduction/Homework1-GetEnvironmentSetUp/src/GetEnvironmentSetUp.java
+++ Week1-CourseIntroduction/Homework1-GetEnvironmentSetUp/src/GetEnvironmentSetUp.java
@@ -1,10 +1,52 @@
-class Student{
-    private unsigned id;
+import edu.truman.cs260.Li.lab.Student;
+import java.util.Scanner;
+import java.util.Vector;
 
-
-}
 public class GetEnvironmentSetUp {
     public static void main(String[] args){
+        Student studentObj1 = new Student();
+        Student studentObj2 = new Student();
+        Student studentObj3 = new Student();
 
+        Scanner name = new Scanner(System.in);
+        Scanner id = new Scanner(System.in);
+
+        System.out.println("What's the name of the student? ");
+        studentObj1.setName(name.nextLine());
+        studentObj2.setName(name.nextLine());
+        studentObj3.setName(name.nextLine());
+        System.out.println("What's the id of the student?");
+        studentObj1.setID(id.nextInt());
+        studentObj2.setID(id.nextInt());
+        studentObj3.setID(id.nextInt());
+
+        Vector<Integer> studentIDVector = new Vector<Integer>();
+        studentIDVector.addElement(studentObj1.getID());
+        studentIDVector.addElement(studentObj2.getID());
+        studentIDVector.addElement(studentObj3.getID());
+
+        Vector<String> studentNameVector = new Vector<String>();
+        studentNameVector.addElement(studentObj1.getName());
+        studentNameVector.addElement(studentObj2.getName());
+        studentNameVector.addElement(studentObj3.getName());
+
+        boolean flag = false;
+        int loopCount = 1;
+        int smallestID = 0;
+        String smallestIdName;
+        while(!flag)
+        {
+            if(studentIDVector.elementAt(loopCount) > studentIDVector.elementAt(loopCount -1))
+            {
+                smallestID = studentIDVector.elementAt(loopCount-1);
+                smallestIdName = studentNameVector.elementAt(loopCount-1);
+                flag = true;
+                System.out.println("The lowest id is: " + smallestID + " With corresponding name: " + smallestIdName);
+            }
+            else
+            {
+                loopCount++;
+            }
+        }
     }
 }
---
+++ Week1-CourseIntroduction/Homework1-GetEnvironmentSetUp/src/edu/truman/cs260/Li/lab/Student.java
@@ -0,0 +1,19 @@
+package edu.truman.cs260.Li.lab;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public void setName(String n) {
+        name = n;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setID(int id_param) {
+        id = id_param;
+    }
+    public int getID() {
+        return id;
+    }
+}
