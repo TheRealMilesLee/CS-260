e2c05862130498b2e1f11f77cb2112ef18239e29
Serendipity <1789216340lhy@gmail.com>
2021-08-28 23:05:47 -0500

    update the comment

--- Week1-CourseIntroduction/Homework1-GetEnvironmentSetUp/src/LaboLi.java
+++ Week1-CourseIntroduction/Homework1-GetEnvironmentSetUp/src/LaboLi.java
@@ -4,29 +4,34 @@ import java.util.Vector;
 
 public class LaboLi {
     public static void main(String[] args) {
+    	// Create three student object for processing.
         Student studentObj1 = new Student();
         Student studentObj2 = new Student();
         Student studentObj3 = new Student();
 
+        // Be ready to input data
         Scanner name = new Scanner(System.in);
         Scanner id = new Scanner(System.in);
 
+        // Get the name of the student.
         System.out.println("What's the name of the student? ");
         studentObj1.setName(name.nextLine());
         studentObj2.setName(name.nextLine());
         studentObj3.setName(name.nextLine());
         
+        // Get the id of the student.
         System.out.println("What's the id of the student?");
         studentObj1.setID(id.nextInt());
         studentObj2.setID(id.nextInt());
         studentObj3.setID(id.nextInt());
       
-
+        // Create two vector object for indexing
         Vector<Integer> studentIDVector = new Vector<Integer>();
         studentIDVector.addElement(studentObj1.getID());
         studentIDVector.addElement(studentObj2.getID());
         studentIDVector.addElement(studentObj3.getID());
 
+        
         Vector<String> studentNameVector = new Vector<String>();
         studentNameVector.addElement(studentObj1.getName());
         studentNameVector.addElement(studentObj2.getName());
@@ -36,7 +41,10 @@ public class LaboLi {
         int smallestID;
         int compareIndex = 0;
         String smallestIdName;
-
+        
+        // If the first student id is smaller than the second, then compare the first student id with third student id. 
+	        // If the first student id is smaller than the third student, then the smallest id is the first student.
+	        // If the first student id is larger than the third student, then the smallest id is the third student.
         if (studentIDVector.elementAt(VectorIndex) < studentIDVector.elementAt(VectorIndex + 1)) {
             compareIndex = studentIDVector.elementAt(VectorIndex);
             if (compareIndex < studentIDVector.elementAt(VectorIndex + 2)) {
@@ -50,6 +58,10 @@ public class LaboLi {
                 System.out.println("\nThe lowest id is: " + smallestID + " With corresponding name: " + smallestIdName);
             }
         }
+        
+        // If the first student id is larger than the second student id, then compare the second student id with the third student id.
+	        // If third student id is smaller than second student id, then the smallest id is the third student.
+	        // If second student id is larger than third student id, then the smallest id is the second student.
         else if(studentIDVector.elementAt(VectorIndex) > studentIDVector.elementAt(VectorIndex + 1)){
             compareIndex = studentIDVector.elementAt(VectorIndex + 1);
             if (compareIndex < studentIDVector.elementAt(VectorIndex + 2)) {
@@ -63,6 +75,7 @@ public class LaboLi {
                 System.out.println("\nThe lowest id is: " + smallestID + " With corresponding name: " + smallestIdName);
             }
         }
+        // Close the file to prevent the leak.
         name.close();
         id.close();
     }
--- Week1-CourseIntroduction/Homework1-GetEnvironmentSetUp/src/edu/truman/cs260/Li/lab/Student.java
+++ Week1-CourseIntroduction/Homework1-GetEnvironmentSetUp/src/edu/truman/cs260/Li/lab/Student.java
@@ -1,18 +1,38 @@
 package edu.truman.cs260.Li.lab;
 
+/**
+ * This class is to manage the student id and the name of the student.
+ * @author Hengyi Li.
+ */
 public class Student {
     private int id;
     private String name;
 
+    /**
+     * This function is to set the name of the student.
+     * @param n is the name of the student.
+     */
     public void setName(String n) {
         name = n;
     }
+    /**
+     * This function is to get the name of the student.
+     * @return the name of the student.
+     */
     public String getName() {
         return name;
     }
+    /**
+     * This function is to set the id of the student.
+     * @param id_param is the id of the student
+     */
     public void setID(int id_param) {
         id = id_param;
     }
+    /**
+     * this function is to get the id of the student.
+     * @return the id of the student.
+     */
     public int getID() {
         return id;
     }
