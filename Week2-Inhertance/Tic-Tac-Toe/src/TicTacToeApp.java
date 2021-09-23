45a40cf38da71ed26fc781b403371dd28370e748
Serendipity <1789216340lhy@gmail.com>
2021-09-23 17:11:58 -0500

    Finished package import

--- Week2-Inhertance/Tic-Tac-Toe/src/TicTacToeApp.java
+++ Week2-Inhertance/Tic-Tac-Toe/src/TicTacToeApp.java
@@ -1,3 +1,7 @@
+import com.edu.truman.li.cs260.Board;
+import com.edu.truman.li.cs260.CompPlayer;
+import com.edu.truman.li.cs260.HumanPlayer;
+import com.edu.truman.li.cs260.Player;
 public class TicTacToeApp {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
