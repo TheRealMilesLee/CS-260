08d7dbf5dfec5602e4e02393a692d2c2f14e8585
Serendipity <1789216340lhy@gmail.com>
2021-10-23 21:31:23 -0500

    Update the main program

--- Lab3-UnionTest/UnionTestInheritance/src/Inhertance.java
+++ Lab3-UnionTest/UnionTestInheritance/src/Inhertance.java
@@ -4,7 +4,7 @@ public class Inhertance
 
   public static void main(String[] args)
   {
-    Card cardObject = new Card(4,"Diamonds");
+    Card cardObject = new Card("Queen","Diamonds");
     System.out.println(cardObject.getRank());
     System.out.println(cardObject.getRankString());
     System.out.println(cardObject.getSuit());
@@ -16,7 +16,18 @@ public class Inhertance
     System.out.println(DigitCardObject.getSuit());
     System.out.println();
     
-   // Card Joker = new Card(1,"None");
+    Card Joker = new Card(1,"None");
+    System.out.println(Joker.getRank());
+    System.out.println(Joker.getRankString());
+    System.out.println(Joker.getSuit());
+    System.out.println();
+    
+    System.out.println("The following is the QueenDiamons card compare to 4Spades Card,"
+        + "negative is less than, positive is greater than and 0 is equal");
+    System.out.println(cardObject.compareTo(DigitCardObject));
+    
+    System.out.println("The following is the Joker card compare to QueenDiamons Card,"
+        + "negative is less than, positive is greater than and 0 is equal");
     System.out.println(cardObject.compareTo(DigitCardObject));
   }
 
