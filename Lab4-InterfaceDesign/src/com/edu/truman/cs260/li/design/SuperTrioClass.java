c6e3901d8b669da0e0ae6a927dac0899cd41d821
Serendipity <1789216340lhy@gmail.com>
2021-11-06 17:46:09 -0500

    Update the comments

--- Lab4-InterfaceDesign/src/com/edu/truman/cs260/li/design/SuperTrioClass.java
+++ Lab4-InterfaceDesign/src/com/edu/truman/cs260/li/design/SuperTrioClass.java
@@ -1,11 +1,10 @@
 package com.edu.truman.cs260.li.design;
 import java.util.Vector;
 /**
- * This class should contain three Strings. ToList() should return a
- * Vector<String> object. Max() should return the String that is at least as
- * large as the other two When compared using the compareTo() method of the
- * String class.
- *
+ * This class should contain three Strings.
+ * ToList() should return a Vector<String> object.
+ * Max() should return the String that is at least as large as the other two.
+ * When compared using the compareTo() method of the String class.
  * @author arkia
  *
  */
