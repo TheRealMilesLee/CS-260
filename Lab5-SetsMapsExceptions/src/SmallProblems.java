edfa415fac46ccbd2757898db5aaea0d796116cb
Serendipity <1789216340lhy@gmail.com>
2021-11-18 18:09:34 -0600

    Update spaces

--- Lab5-SetsMapsExceptions/src/SmallProblems.java
+++ Lab5-SetsMapsExceptions/src/SmallProblems.java
@@ -2,14 +2,10 @@ import edu.truman.li.cs260.lab5.*;
 
 public class SmallProblems
 {
-  
   public static void main(String[] args)
   {
     Frequency frequencyObject = new Frequency();
     Dictionary dictionaryObject = new Dictionary();
     SetOperation setObject = new SetOperation();
-    
-    
   }
-  
 }
